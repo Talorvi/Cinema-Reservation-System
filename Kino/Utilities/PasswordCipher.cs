b7b14f15cd1ef5d3ad71c3d90ac73a16441d8e8c
tomanoo <tomasz.weclawski96@gmail.com>
2020-10-22 20:24:49 +0200

    Added standard password ciphering

--- Kino/Utilities/PasswordCipher.cs
+++ Kino/Utilities/PasswordCipher.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
 namespace Kino.Utilities
 {
     class PasswordCipher
     {
+        // standard ROT47 converter
+        private static char ConvertChar(char c)
+        {
+            return c > 32 && c < 127
+                ? (char)(33 + ((c + 14) % 94))
+                : c;
+        }
+
+        public static string ConvertPassword(string password)
+        {
+            StringBuilder encryptedPassword = new StringBuilder();
+
+            for (int i = 0; i < password.Length; i++)
+            {
+                encryptedPassword.Append(ConvertChar(password[i]));
+            }
+
+            return encryptedPassword.ToString();
+        }
     }
 }
