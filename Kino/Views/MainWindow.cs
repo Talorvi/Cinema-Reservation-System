f04347389bcee48b2340b60e64fc4b7d03a51bd4
xGumis <39995960+xGumis@users.noreply.github.com>
2020-10-23 11:48:28 +0200

    win man in main win

---
+++ Kino/Views/MainWindow.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Forms;
+
+namespace Kino.Views
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void ChangeWindow(Control control) 
+        {
+            panelMain.Controls.Clear();
+            control.Dock = DockStyle.Fill;
+            panelMain.Controls.Add(control);
+        }
+    }
+}
