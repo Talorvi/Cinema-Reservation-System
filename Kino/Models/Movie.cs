83721fad31a2178547bd8e2f2f46fdb2b82f4853
Mateusz Siedlarz <smateusz96@gmail.com>
2020-10-23 14:08:27 +0200

    added test library for DB connection

---
+++ Kino/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using LinqToDB;
+using LinqToDB.Mapping;
+
+namespace Kino.Models
+{
+    [Table(Name = "movies")]
+    public class Movie
+    {
+        [PrimaryKey, Identity]
+        public int Id { get; set; }
+
+        [Column(Name = "name"), NotNull]
+        public string Name { get; set; }
+    }
+    
+    public class DbCinema : LinqToDB.Data.DataConnection
+    {
+        public DbCinema() : base("Cinema") { }
+        public ITable<Movie> Movies { get { return GetTable<Movie>(); } }
+    }
+}
