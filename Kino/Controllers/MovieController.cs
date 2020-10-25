a458633e8ccaa6cde3f4c62d682e9cfa8aa6b706
Mateusz Siedlarz <smateusz96@gmail.com>
2020-10-25 11:35:43 +0100

    added Hall.cs model added HallController.cs

--- Kino/Controllers/MovieController.cs
+++ Kino/Controllers/MovieController.cs
@@ -10,67 +10,57 @@ namespace Kino.Controllers
     {
         public static List<Movie> All()
         {
-            using (var db = new DbCinema())
-            {
-                var query = from movie in db.Movies
-                    orderby movie.Name descending
-                    select movie;
-                var movies = query.ToList();
-                return movies;
-            }
+            using var db = new DbCinema();
+            var query = from movie in db.Movies
+                orderby movie.Name descending
+                select movie;
+            var movies = query.ToList();
+            return movies;
         }
 
         public static Movie Get(int id)
         {
-            using (var db = new DbCinema())
-            {
-                var query = from movie in db.Movies
-                    where movie.Id == id
-                    select movie;
-                var singleMovie = query.ToList()[0];
-                return singleMovie;
-            }
+            using var db = new DbCinema();
+            var query = from movie in db.Movies
+                where movie.Id == id
+                select movie;
+            var singleMovie = query.ToList()[0];
+            return singleMovie;
         }
 
         public static void Add(string name, double length)
         {
-            using (var db = new DbCinema())
-            {
-                db.Movies
-                    .Value(movie => movie.Name, name)
-                    .Value(movie => movie.Length, length)
-                    .Insert();
-            }
+            using var db = new DbCinema();
+            db.Movies
+                .Value(movie => movie.Name, name)
+                .Value(movie => movie.Length, length)
+                .Insert();
         }
 
         public static void Update(int id, string? name, double? length)
         {
-            using (var db = new DbCinema())
+            using var db = new DbCinema();
+            var movie = Get(id);
+
+            if (name != null)
             {
-                var movie = Get(id);
-
-                if (name != null)
-                {
-                    movie.Name = name;
-                }
-
-                if (length != null)
-                {
-                    movie.Length = (double) length;
-                }
-
-                db.Update(movie);
+                movie.Name = name;
             }
+
+            if (length != null)
+            {
+                movie.Length = (double) length;
+            }
+
+            db.Update(movie);
         }
         
         public static void Delete(int id)
         {
-            using (var db = new DbCinema())
-            {
-                db.Movies
-                    .Where(movie => movie.Id == id)
-                    .Delete();
-            }
+            using var db = new DbCinema();
+            db.Movies
+                .Where(movie => movie.Id == id)
+                .Delete();
         }
     }
 }
--- Kino/Models/DbCinema.cs
+++ Kino/Models/DbCinema.cs
@@ -2,9 +2,13 @@
 
 namespace Kino.Models
 {
+    /*
+     * Model that allows connecting to DB and operating on it
+     */
     public class DbCinema : LinqToDB.Data.DataConnection
     {
         public DbCinema() : base("Cinema") { }
         public ITable<Movie> Movies => GetTable<Movie>();
+        public ITable<Hall> Halls => GetTable<Hall>();
     }
 }
