d6925f43358752ffafd66411f2f7ba62a90d84e0
Mateusz Siedlarz <smateusz96@gmail.com>
2020-11-02 16:38:43 +0100

    added few viemodels

---
+++ Kino/ViewModels/HallViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Kino.Utilities;
+using Kino.Validators;
+using Kino.Views.Interfaces;
+
+namespace Kino.ViewModels
+{
+    class HallViewModel
+    {
+        private IHall view;
+        public HallViewModel(IHall view)
+        {
+            this.view = view;
+            view.SaveHall += SaveHall;
+            view.GetHallName += GetHallName;
+            view.SetHallName += SetHallName;
+        }
+
+        private void SaveHall()
+        {
+            if (Cache.Hall.Id != 0)
+            {
+                HallValidator.HallUpdateValidation(Cache.Hall.Id, Cache.Hall.Name);
+            }
+            else
+            {
+                HallValidator.HallAddValidation(Cache.Hall.Name);
+            }
+        }
+
+        private string GetHallName()
+        {
+            return Cache.Hall.Name;
+        }
+
+        private void SetHallName(string name)
+        {
+            Cache.Hall.Name = name;
+        }
+    }
+}
